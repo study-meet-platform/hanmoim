6e0a68b7bc4272f9a3e0b07b0aecc53f424a7a89
1000chw <1000chw@gmail.com>
2025-03-16 15:39:55 +0900

    feat: Users Apis change by UserId decorator

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -8,38 +8,39 @@ import {
   Delete,
   UseInterceptors,
   ClassSerializerInterceptor,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { DeleteResult, UpdateResult } from 'typeorm';
+import { UserId } from '../decorators/user-id.decorator';
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
+  @Get('all')
   async findAll(): Promise<User[]> {
     return await this.usersService.findAll();
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<User> {
-    return await this.usersService.findOne(+id);
+  @Get()
+  async findOne(@UserId(ParseIntPipe) userId: number): Promise<User> {
+    return await this.usersService.findOne(+userId);
   }
 
-  @Patch(':id')
+  @Patch()
   async update(
-    @Param('id') id: string,
+    @UserId(ParseIntPipe) userId: number,
     @Body() updateUserDto: UpdateUserDto,
   ): Promise<UpdateResult> {
-    return await this.usersService.update(+id, updateUserDto);
+    return await this.usersService.update(+userId, updateUserDto);
   }
 
-  @Delete(':id')
-  async remove(@Param('id') id: string): Promise<DeleteResult> {
-    return await this.usersService.remove(+id);
+  @Delete()
+  async remove(@UserId(ParseIntPipe) userId: number): Promise<DeleteResult> {
+    return await this.usersService.remove(+userId);
   }
 }
