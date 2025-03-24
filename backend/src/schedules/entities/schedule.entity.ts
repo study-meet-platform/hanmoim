22e0710b607a20fab43193d469165c1ad61ef172
김명준 <mjk3482@gmail.com>
2025-03-24 15:40:23 +0900

    feat: schedule entity
    일정 대상자를 제외한 일정 엔티티 작성, roomId join 부분 & eventTime의 날짜/타임스탬프 부분 고려할 필요 있음

--- backend/src/schedules/entities/schedule.entity.ts
+++ backend/src/schedules/entities/schedule.entity.ts
@@ -1,1 +1,47 @@
-export class Schedule {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString } from 'class-validator';
+import { Exclude } from 'class-transformer';
+
+@Entity('schedules')
+export class Schedule {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  @IsNumber()
+  scheduleId: number;
+
+  @ManyToOne(() => Room, { eager: false })
+  @JoinColumn({ name: 'roomId' })
+  room: Room;
+
+  @ApiProperty()
+  @Column()
+  @IsNumber()
+  roomId: number;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @CreateDateColumn()
+  @IsDate()
+  createdAt: string;
+
+  @ApiProperty()
+  @IsDate()
+  eventTime: string;
+}
